9be36fa250348feed83dd091efc9b7d54e6699d7
antoniab79 <antonia@webleviathan.com>
2024-09-14 01:20:32 +0100

    added repo url in footer

--- src/App.js
+++ src/App.js
@@ -6,6 +6,26 @@ function App() {
   return (
     <div className="App">
         Weather Application
+        <footer>  
+        This code is hosted on GitHub and is {" "}
+          <a
+            className="App-link"
+            href="https://github.com/antoniab79/react-weather-app"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            open sourced
+          </a>
+          ; {" "}coded by{" "}
+          <a
+            className="App-link"
+            href="https://www.shecodes.io/graduates/109902-antonia-bezinovic"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Antonia Bezinovic
+          </a>
+          </footer>
     </div>
   );
 }
