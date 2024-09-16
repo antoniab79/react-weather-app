56cbd6582c0dd75f37942ac6a6678db0c3643a78
antoniab79 <antonia@webleviathan.com>
2024-09-16 22:49:08 +0100

    removed unnecessary  component

--- src/Weather.js
+++ src/Weather.js
@@ -1,6 +1,5 @@
 import React, { useState } from "react";
 import WeatherInfo from "./WeatherInfo";
-import UnitConversion from "./UnitConversion";
 import axios from "axios";
 import "./Weather.css";
 
