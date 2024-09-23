5943f1eb4eff9986a25c1170c4efcc46761932c0
antoniab79 <antonia@webleviathan.com>
2024-09-23 18:02:54 +0100

    added 5 day weather forecast component and day component

--- src/Weather.js
+++ src/Weather.js
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import WeatherInfo from "./WeatherInfo";
+import WeatherForecast from "./WeatherForecast";
 import axios from "axios";
 import "./Weather.css";
 
@@ -49,6 +50,7 @@ export default function Weather(props) {
                 </div>
                 </form>
                 <WeatherInfo data={weatherData} />
+                <WeatherForecast data={weatherData} />
                 </div>
         );
     } else  {
---
+++ src/WeatherForecast.js
@@ -0,0 +1,45 @@
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import ForecastDay from "./ForecastDay";
+
+export default function WeatherForecast(props) {
+    let [loaded, setLoaded] = useState(false);
+    let [forecast, setForecast] = useState(null);
+
+    useEffect(() => {
+        setLoaded(false);
+    }, [props.data.city]);
+
+    function handleResponse(response) {
+        setForecast(response.data.daily);
+        setLoaded(true);
+    }
+
+    if (loaded) {
+        return (
+            <div className="WeatherForecast">
+                <div className="row">
+                {forecast.map(function (dailyForecast, index) {
+                        if (index < 5) {
+                            return (
+                                <div className="col" key={index}>
+                                    <ForecastDay temp={dailyForecast} />
+                                </div>
+                            );
+                        } else {
+                            return null;
+                        }
+                    })}
+                </div>
+            </div>
+        );
+    } else {
+        const apiKey = "1e5e2f20350d3toa44fe9d489eb954a7";
+        let city = props.data.city;
+        let apiUrl = `https://api.shecodes.io/weather/v1/forecast?query=${city}&key=${apiKey}&units=metric`;
+        
+        axios.get(apiUrl).then(handleResponse);
+
+        return null;
+    }
+}
